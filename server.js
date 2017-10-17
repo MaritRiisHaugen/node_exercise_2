699cfc88453f4475df28848ffea7f3a2a00383c9
unknown <maritriishaugen@gmail.com>
2017-10-17 13:07:56 +0200

    modul 2 ferdig tror jeg

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+
+/*app.get('/', function (req, res) {
+  res.send('Hello World!')
+})*/
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
+
+
+
+var greetings = ["Greetings", "Hello", "Salutations", "Hi", "Good day" ];
+
+var random = Math.floor(Math.random()*(greetings.length+1));
+
+var name = [" Christian"];
+
+app.get('/greet', function (req, res) {
+  res.send(greetings[random] + " " + name[0]);
+})
